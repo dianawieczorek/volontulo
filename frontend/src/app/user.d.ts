f6d4411c1f6a1015d52335ffbbcdfd58edd38b61
dianawieczorek <wieczorek.diana@outlook.com>
2018-07-04 18:56:49 +0200

    made first & last name unsplited

--- frontend/src/app/user.d.ts
+++ frontend/src/app/user.d.ts
@@ -4,4 +4,8 @@ export interface User {
   isAdministrator: boolean,
   organizations: Array<Organization>,
   username: string,
+  firstName: string,
+  lastName: string,
+  email: string,
+  phoneNo: string;
 }
