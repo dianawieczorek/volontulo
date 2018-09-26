fab3fbe966016b403f4ee09cbfd39dccf011357f
dianawieczorek <wieczorek.diana@outlook.com>
2018-09-26 13:29:49 +0200

    changed custom facebook metatags for organizations

--- frontend/src/app/organization/organization-details/organization-details.component.ts
+++ frontend/src/app/organization/organization-details/organization-details.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import {Component, Inject, Input} from '@angular/core';
 
-import { Organization } from '../organization.model';
-import { environment } from '../../../environments/environment';
+import {Organization} from '../organization.model';
+import {environment} from '../../../environments/environment';
+import {Meta} from "@angular/platform-browser";
 
 @Component({
   selector: 'volontulo-organization-details',
@@ -13,4 +14,14 @@ export class OrganizationDetailsComponent {
   @Input() isUserOrgMember: boolean;
   @Input() organization: Organization;
   djangoRoot: string = environment.djangoRoot;
+
+  constructor(private metaService: Meta)
+   {
+   }
+
+  ngOnInit() {
+    this.metaService.updateTag({ property: 'og:title', content: this.organization.name + '- Volontulo. Portal dla wolontariuszy' });
+    this.metaService.updateTag({ property: 'og:description', content: this.organization.description});
+  }
+
 }
