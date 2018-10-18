2934055892b3f2997694381d58322af1980e5274
dianawieczorek <wieczorek.diana@outlook.com>
2018-10-18 09:32:49 +0200

    used metatagsService in org-details-component, changed spaces, changed name to property in metatags.service

--- frontend/src/app/organization/organization-details/organization-details.component.ts
+++ frontend/src/app/organization/organization-details/organization-details.component.ts
@@ -1,8 +1,8 @@
-import {Component, Inject, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
-import {Organization} from '../organization.model';
-import {environment} from '../../../environments/environment';
-import {Meta} from "@angular/platform-browser";
+import { Organization } from '../organization.model';
+import { environment } from '../../../environments/environment';
+import { MetatagsService } from "../../metatags.service";
 
 @Component({
   selector: 'volontulo-organization-details',
@@ -15,13 +15,13 @@ export class OrganizationDetailsComponent {
   @Input() organization: Organization;
   djangoRoot: string = environment.djangoRoot;
 
-  constructor(private metaService: Meta)
-  {
-  }
+  constructor(private metatagsService: MetatagsService) { }
 
   ngOnInit() {
-    this.metaService.updateTag({ property: 'og:title', content: this.organization.name + ' - Volontulo. Portal dla wolontariuszy' });
-    this.metaService.updateTag({ property: 'og:description', content: this.organization.description});
+    this.metatagsService.setMeta('Volontulo. Portal dla wolontariuszy', {
+        'og:title': this.organization.name + ' - Volontulo. Portal dla wolontariuszy',
+        'og:description': this.organization.description
+      }
+    );
   }
-
 }
